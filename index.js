38615fb64deb60ba2b57a1534043358acf06f0f1
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-01-21 18:59:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function moveDodgerLeft() {
+    let dodger = document.getElementById('dodger');
+
+    var left = dodger.style.left;
+    left = parseInt(left);
+    var newLeft = left - 5;
+    console.log('left', left);
+    console.log('newLeft', newLeft);
+
+    dodger.style.left = newLeft + 'px';
+}
+
+function moveDodgerRight() {
+    let dodger = document.getElementById('dodger');
+
+    var left = dodger.style.left;
+    left = parseInt(left);
+    var newLeft = left + 5;
+    console.log('left', left);
+    console.log('newLeft', newLeft);
+
+    dodger.style.left = newLeft + 'px';
+}
